4a4688804b66c2a9e46c5349cb648fa1915ace2a
George <george86.dev@gmail.com>
2024-12-01 21:14:39 +0100

    Add restaurant form feature

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,26 @@
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+  useUpdateMyRestaurant,
+} from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  const { createRestaurant, isPending: isCreateLoading } =
+    useCreateMyRestaurant();
+  const { restaurant, isLoading } = useGetMyRestaurant();
+  const { updateRestaurant, isPending: isUpdateLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
+
+  return (
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
+    />
+  );
+};
+
+export default ManageRestaurantPage;
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,23 @@ export type User = {
   city: string;
   country: string;
 };
+
+export type MenuItem = {
+  _id: string;
+  name: string;
+  price: number;
+};
+
+export type Restaurant = {
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  countr: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
+  lastUpdated: string;
+};
